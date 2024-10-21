b8a6ec6e4e5d12849a69c151c1ec10bd790e34ba
eshan-009 <eshansharma009@gmail.com>
2024-10-21 11:42:52 +0530

    Eshan 21-10-24

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,17 @@ import HomePage from './pages/Home/HomePage'
 
 import NavBar from './components/NavBar/NavBar'
 import CreateOrganization from './pages/Home/Organization/CreateOrganization'
+import OrganizationList from './pages/Home/Organization/OrganizationList'
+import CreateSubOrganization from './pages/Home/SubOrganization/CreateSubOrganization'
+import SubOrganizationList from './pages/Home/SubOrganization/SubOrganizationList'
+import CreateRole from './pages/Home/Role/CreateRole'
+import RoleList from './pages/Home/Role/RoleList'
+import CreateEmployee from './pages/Home/Employee/CreateEmployee'
+import EmployeeList from './pages/Home/Employee/EmployeeList'
+import CreateDepartment from './pages/Home/Department/CreateDepartment'
+import DepartmentList from './pages/Home/Department/DepartmentList'
+
+
 function App() {
  
 
@@ -17,6 +28,19 @@ function App() {
     <Route path="/home" element={<HomePage/>}>
     
     <Route path='/home/Create-Organization' element={<CreateOrganization/>}></Route>
+    <Route path='/home/Organization-List' element={<OrganizationList/>}></Route>
+
+    <Route path='/home/Create-Sub-Organization' element={<CreateSubOrganization/>}></Route>
+    <Route path='/home/Sub-Organization-List' element={<SubOrganizationList/>}></Route>
+
+    <Route path='/home/Create-Department' element={<CreateDepartment/>}></Route>
+    <Route path='/home/Department-List' element={<DepartmentList/>}></Route>
+
+    <Route path='/home/Create-Role' element={<CreateRole/>}></Route>
+    <Route path='/home/Role-List' element={<RoleList/>}></Route>
+
+    <Route path='/home/Create-Employee' element={<CreateEmployee/>}></Route>
+    <Route path='/home/Employee-List' element={<EmployeeList/>}></Route>
     
     </Route>
     
